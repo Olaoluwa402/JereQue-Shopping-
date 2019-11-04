8d376c83a128ee2e841fe56bc7031dde55dd58de
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2019-11-04 13:47:30 +0000

    v2 modified still

--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 const passport = require("passport");
-// const request = require('request');
+const request = require('request');
 const _ = require('lodash');
 
 
