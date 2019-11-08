373ff2a8e1cafbefd678ab428a43fe886d741753
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2019-11-08 07:47:47 +0000

    some form styling

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
-require('dotenv').config();
-
-console.log(process.env);
-
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 
 const express = require("express");
 const app = express();
