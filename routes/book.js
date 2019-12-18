8a2e1b1789993c380ee03f66bbb5b2d13fe60753
Olaoluwa IBUKUN <ibukundaniel402@yahoo.co.uk>
2019-12-18 00:25:57 +0000

    some changes

--- routes/book.js
+++ routes/book.js
@@ -74,10 +74,10 @@ cloudinary.config({
 // All books page
 // Book.find().populate({'path': 'category', match:{'name':'Medical'}})
 router.get("/", async (req, res, next) => {
-  const limit = await BookLimit.findOne({});
-  const limitResult = parseInt(limit.book_limit);
+  // const limit = await BookLimit.findOne({});
+  // const limitResult = parseInt(limit.book_limit);
 
-  let query = Book.find().sort({createdAt: 'desc'}).limit(limitResult);
+  let query = Book.find().sort({createdAt: 'desc'}).limit(12);
   if (req.query.book_title != null && req.query.book_title != ''){
     query = query.regex('book_title', new RegExp(req.query.book_title, 'i'));
   }
